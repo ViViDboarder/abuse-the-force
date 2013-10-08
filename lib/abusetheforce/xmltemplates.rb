9ab1eb3b986f0c4a8d30ab0c8a8f90034f86b545
ViViDboarder <ViViDboarder@gmail.com>
2013-10-07 17:48:30 -0700

    Fix to whitespace formatting in static resource xml template

--- lib/abusetheforce/xmltemplates.rb
+++ lib/abusetheforce/xmltemplates.rb
@@ -2,7 +2,7 @@ module AbuseTheForce
 
     XML_STATIC_RESOURCE = <<-eos
 <?xml version="1.0" encoding="UTF-8"?>
-    <StaticResource xmlns="http://soap.sforce.com/2006/04/metadata">
+<StaticResource xmlns="http://soap.sforce.com/2006/04/metadata">
     <cacheControl>Public</cacheControl>
     <contentType>application/zip</contentType>
     <description>Static resource uploaded with Abuse the Force</description>
